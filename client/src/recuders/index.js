1db04a9fab2deb42ec4c6035a12502662ad7636f
kicky1 <krzysiekwicki1@gmail.com>
2021-05-03 23:11:14 +0200

    Dodanie formularza wypeniania danych graczy

--- client/src/recuders/index.js
+++ client/src/recuders/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
 
-import posts from './posts';
+import players from './players';
 
-export const reducers = combineReducers({ posts });
+export const reducers = combineReducers({ players });
